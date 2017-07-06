e28e9095438b727af2ad032341bac34c92023119
Nicolas Stohler <nicolas.stohler@ensi.ch>
2017-07-06 16:50:47 +0200

    demo code on how to retrieve a list of items via HTTP GET, convert JSON data to cs object array/list

--- ApsNetCoreConsole/AdUserInfoListItem.cs
+++ ApsNetCoreConsole/AdUserInfoListItem.cs
@@ -1,20 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ApsNetCoreConsole
 {
-    public class AdUserInfo
+    public class AdUserInfoListItem
     {
         public string DisplayName { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string KurzZeichen { get; set; }
         public string SamAccountName { get; set; }
-        public string Mail { get; set; }
-        public string TelephoneNumber { get; set; }
+        public string Sektion { get; set; }
     }
 }
--- ApsNetCoreConsole/Program.cs
+++ ApsNetCoreConsole/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
@@ -30,53 +32,97 @@ namespace ApsNetCoreConsole
             // Windows authentication for HttpClient:
             // add "new HttpClientHandler() { UseDefaultCredentials = true })" to ctor call
 
-            using (var client = new HttpClient(new HttpClientHandler() { UseDefaultCredentials = true }))            
+            using (var client = new HttpClient(new HttpClientHandler() { UseDefaultCredentials = true }))
             {
                 // http://www.binaryintellect.net/articles/6903087e-5e5c-445a-be20-cee0a7eb434e.aspx                
                 client.BaseAddress = new Uri("https://e200681-web01.ensi.ch:44301/");
                 var contentType = new MediaTypeWithQualityHeaderValue("application/json");
                 client.DefaultRequestHeaders.Accept.Add(contentType);
 
-                //----------------------
-                // GET api/adinfo/ping             
-                var response = await client.GetAsync("api/adinfo/ping");
-                if (response.IsSuccessStatusCode)
-                {
-                    var stringData = await response.Content.ReadAsStringAsync();
-                    
-                    // convert json data to c# object:
-                    var pingResult = JsonConvert.DeserializeObject<PingResult>(stringData);
+                var pingResult = await ExecuteGetPing(client);
+                var adUserInfo = await ExecuteGetAdInfo(client);
+                var adUserInfoList = await ExecuteGetAdInfoList(client);
+            }
+        }
 
-                    // output c# object to console
-                    Console.WriteLine($"pingResult = {JsonConvert.SerializeObject(pingResult)}");   
-                }
-                else
-                {
-                    throw new Exception("some stupid error during http request");
-                }
+        private static async Task<PingResult> ExecuteGetPing(HttpClient client)
+        {
+            //----------------------
+            // GET api/adinfo/ping             
+            var response = await client.GetAsync("api/adinfo/ping");
+            if (response.IsSuccessStatusCode)
+            {
+                var stringData = await response.Content.ReadAsStringAsync();
 
-                //----------------------
-                // GET api/adinfo             
-                response = await client.GetAsync("api/adinfo");
-                if (response.IsSuccessStatusCode)
-                {
-                    var stringData = await response.Content.ReadAsStringAsync();
+                // convert json data to c# object:
+                var pingResult = JsonConvert.DeserializeObject<PingResult>(stringData);
 
-                    // use "data" node to convert to c# object:
-                    // https://stackoverflow.com/a/29702862/54159
-                    var parsed = JObject.Parse(stringData);
-                    var data = parsed["data"];
+                // output c# object to console
+                Console.WriteLine($"pingResult = {JsonConvert.SerializeObject(pingResult)}");
 
-                    // convert json data to c# object:
-                    var adUserInfo = JsonConvert.DeserializeObject<AdUserInfo>(data.ToString());
+                return pingResult;
+            }
+            else
+            {
+                throw new Exception("some stupid error during http request");
+            }            
+        }
 
-                    // output c# object to console
-                    Console.WriteLine($"adUserInfo = {JsonConvert.SerializeObject(adUserInfo)}");
-                }
-                else
-                {
-                    throw new Exception("some stupid error during http request");
-                }
+        private static async Task<AdUserInfo> ExecuteGetAdInfo(HttpClient client)
+        {
+            //----------------------
+            // Current User
+            // GET api/adinfo             
+            var response = await client.GetAsync("api/adinfo");
+            if (response.IsSuccessStatusCode)
+            {
+                var stringData = await response.Content.ReadAsStringAsync();
+
+                // use "data" node to convert to c# object:
+                // https://stackoverflow.com/a/29702862/54159
+                var parsed = JObject.Parse(stringData);
+                var data = parsed["data"];
+
+                // convert json data to c# object:
+                var adUserInfo = JsonConvert.DeserializeObject<AdUserInfo>(data.ToString());
+
+                // output c# object to console
+                Console.WriteLine($"adUserInfo = {JsonConvert.SerializeObject(adUserInfo)}");
+
+                return adUserInfo;
+            }
+            else
+            {
+                throw new Exception("some stupid error during http request");
+            }
+        }
+
+        private static async Task<List<AdUserInfoListItem>> ExecuteGetAdInfoList(HttpClient client)
+        {
+            //----------------------
+            // Current User
+            // GET api/adinfo/list/ensi
+            var response = await client.GetAsync("api/adinfo/list/ensi");
+            if (response.IsSuccessStatusCode)
+            {
+                var stringData = await response.Content.ReadAsStringAsync();
+
+                // use "data" node to convert to c# object:
+                // https://stackoverflow.com/a/29702862/54159
+                var parsed = JObject.Parse(stringData);
+                var data = parsed["data"];
+
+                // convert json data to c# object:
+                var adUserInfoListItems = JsonConvert.DeserializeObject<List<AdUserInfoListItem>>(data.ToString());
+
+                // output kurzzeichen to console
+                Console.WriteLine($"adUserInfoList = [{adUserInfoListItems.Count}]: {string.Join(", ", adUserInfoListItems.Select(x => x.KurzZeichen))}");
+
+                return adUserInfoListItems;
+            }
+            else
+            {
+                throw new Exception("some stupid error during http request");
             }
         }
     }
